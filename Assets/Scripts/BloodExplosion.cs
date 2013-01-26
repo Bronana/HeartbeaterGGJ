28931d27124c2f438c16e5488206fc62da3b47ca
Mike Geig <mikegeig@gmail.com>
2013-01-26 10:53:24 -0500

    Finished blood effects

---
+++ Assets/Scripts/BloodExplosion.cs
@@ -0,0 +1,38 @@
+using UnityEngine;
+using System.Collections;
+
+public class BloodExplosion : MonoBehaviour {
+	
+	public GameObject blood;
+	private bool destroyed  = false;
+	private int deadCounter = 0;
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+		if(destroyed)
+		{
+			if(++deadCounter == 10)
+				Destroy(transform.parent.gameObject);
+			else{
+				Color originalColor = renderer.material.color;
+				renderer.material.color = new Color(originalColor.r, originalColor.g, originalColor.b, 1F - deadCounter * .1F);
+			}
+			
+		}
+		
+	}
+	
+	void OnTriggerEnter(Collider other)
+	{
+		destroyed = true;
+		
+		Destroy(collider);
+		
+		Instantiate(blood, new Vector3(transform.position.x, transform.position.y + 1.5F, transform.position.z), Quaternion.identity);
+	}
+}
