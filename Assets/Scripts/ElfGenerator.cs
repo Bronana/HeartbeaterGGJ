3935ead3e641fc6a123e8d43a5e5844d0812845f
Mike Geig <mikegeig@gmail.com>
2013-01-27 13:13:22 -0500

    items added

---
+++ Assets/Scripts/ElfGenerator.cs
@@ -0,0 +1,24 @@
+using UnityEngine;
+using System.Collections;
+
+public class ElfGenerator : MonoBehaviour {
+
+	public GameObject elf;
+	public GameObject player;
+	
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+		if(Random.Range(0F, 300F) < 10F)
+		{
+			//Instantiate(elf, new Vector3(player.transform.position.x + 2F, player.transform.position.y, player.transform.position.z), Quaternion.Euler(0, -90, 0));
+			Instantiate(elf, new Vector3(player.transform.position.x + 10F, player.transform.position.y, player.transform.position.z), Quaternion.identity);
+		}
+		
+	}
+}
